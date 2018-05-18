b019894e3804f36b3bd43076b2a5e5f2fc581b0c
cindych419@gmail.com <cindych419@gmail.com>
2018-05-18 19:25:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,12 +5,12 @@ class Anagram
     @word=word
   end
 
-  def match(%w(some words))
+  def match(some_words)
     some_words=[]
     word=[]
     word=@word.split("").sort
     some_words=%W(some words).split("")
-    if 
+    if
       word.collect{|some_words| some_words.split.sort = word}
   else
     word
